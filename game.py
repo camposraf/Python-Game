8b5dffd64da4c30147bda2a8aa5ec3f0ba66cc3f
Rafael Aurelius Campos <therobotfighter.009@gmail.com>
2026-08-27 08:39:32 +0800

    initial player movement set up

--- game.py
+++ game.py
@@ -20,11 +20,25 @@ def char(x,y):
 
 x = (display_width * 0.45)
 y = (display_height *0.8)
+x_change = 0
+char_speed = 0
 
 while not crashed:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             crashed = True
+
+        #Player movement
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT:
+                x_change = -5
+            elif event.key == pygame.K_RIGHT:
+                x_change = 5
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
+                x_change = 0
+
+    x += x_change
 
     gameDisplay.fill(white)
     char(x,y)
